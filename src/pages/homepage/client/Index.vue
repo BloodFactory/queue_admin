519f0208703e087eb83c44740d9fb3590f8f4fbf
Chukov Dmitry <chukov@marinet.ru>
2021-05-18 14:28:32 +0300

    SAVE

--- src/pages/homepage/client/Index.vue
+++ src/pages/homepage/client/Index.vue
@@ -1,7 +1,17 @@
 <template>
     <q-page padding>
-        <div class="row">
+        <div class="row q-col-gutter-x-xl">
             <UserOrganizationsList v-model="organization"/>
+
+            <div class="col" v-if="organization">
+                <q-card>
+                    <q-card-section>
+                        <div class="text-h5">
+                            {{ organization.label }}
+                        </div>
+                    </q-card-section>
+                </q-card>
+            </div>
         </div>
     </q-page>
 </template>
